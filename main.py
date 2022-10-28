ea61a54d59603602ee0779e2e68ad24306ba4b76
Ethan Zemelman <ethanzemelman1@gmail.com>
2022-10-28 15:17:00 -0400

    create main file to put in custom url/username/password

---
+++ main.py
@@ -0,0 +1,7 @@
+import math_grade_calculator as mc
+
+url = input("Enter the url of the 'My Works' page in your math class: ")
+username = input("Enter your Edsby username: ")
+password = input("Enter your Edsby password: ")
+
+mc.calculate(url, username, password)
