a5481591f917fcea7e94c68d659c15783bb5409e
Ethan Zemelman <ethanzemelman@gmail.com>
2022-10-29 23:40:31 -0400

    Add loading message

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ url = input("Enter the url of the 'My Works' page in your math class: ")
 username = input("Enter your Edsby username: ")
 password = input("Enter your Edsby password: ")
 
+print("Calculating. This may take a few seconds...")
+
 mc.calculate(url, username, password)
